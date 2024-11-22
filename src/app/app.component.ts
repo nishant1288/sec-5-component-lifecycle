2f60a331dee51a0ce2183dfddb8e1e6a1f90d9b5
Nishant Krishna Ghadigaonkar <nishantkrishna1288@gmail.com>
2024-11-22 06:13:42 +0530

    constuctor and ngoninit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'sec-5-component-lifecycle';
+
+  constructor() {
+    console.log('Constructor is called')
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit is called')
+  }
 }
